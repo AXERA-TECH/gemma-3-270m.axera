06539beeddbe92b3aceaabb5f40ddb7a9afe9138
lihongjie <lihongjie@axera-tech.com>
2026-06-11 17:39:28 +0800

    update

--- model_convert/build_all.sh
+++ model_convert/build_all.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# bash "${SCRIPT_DIR}/build_llm.sh"
+bash "${SCRIPT_DIR}/build_llm.sh"
 bash "${SCRIPT_DIR}/prepare_package.sh"
 
